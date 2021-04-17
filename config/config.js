097f54b3fb62eefb6ee0c71d3181fa5e57f6a910
unknown <amdtkj@gmail.com>
2021-04-17 15:29:48 +0700

    change user

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3005,
   db_name : "mpcomm",
-  db_username : "minipro",
-  db_password: "tUmb4L",
+  db_username : "postgres",
+  db_password: "admin",
   db_remote_host : '192.168.100.254'
 }
 export default config
